edba0738518ee2dca6863d3d6b6b78bf786afaf2
Alex Hoffman <alxhoff@gmail.com>
2019-09-12 23:27:25 +0800

    Fixed jumping and collision blocks

--- Player.gd
+++ Player.gd
@@ -5,33 +5,58 @@ extends KinematicBody2D
 # var b = "text"
 
 const UP = Vector2(0,-1)
-const GRAVITY = 20
-const SPEED = 200
+const GRAVITY = 15
+const MAX_SPEED = 200
+const ACCELERATION = 20
+const GROUND_FRICTION = 0.25
+const AIR_FRICTION = 0.05
 const JUMP_HEIGHT = -750
 
-
 var motion = Vector2()
+var jumping = false
 
 func _physics_process(delta):
+	var friction = false
+
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
-		$Sprite.play("Running")
 	elif Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
-		$Sprite.play("Running")
-		motion.x = -SPEED
-	else:
-		$Sprite.play("Idle")
-		motion.x = 0
+	
 
 	if is_on_floor():
+		if Input.is_action_pressed("ui_right"):
+			$Sprite.play("Running")
+		elif Input.is_action_pressed("ui_left"):
+			$Sprite.play("Running")
+		else:
+			$Sprite.play("Idle")
+			friction = true
+		
+		if jumping:
+			jumping = false
+			$Sprite.play("Landing")
+
+			
 		if Input.is_action_pressed("ui_up"):
+			jumping = true
 			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, GROUND_FRICTION)
 	else:
-		$Sprite.play("Jumping")
+		if motion.y > 0:
+			$Sprite.play("Falling")
+
+		elif motion.y < 0:
+			$Sprite.play("Jumping")
+
+		
+		if friction == true:
+			motion.x = lerp(motion.x, 0, AIR_FRICTION)
 
 	motion = move_and_slide(motion, UP)
 
